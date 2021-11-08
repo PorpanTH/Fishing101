cc7148062ce3ab387327dd2015a31ae960ecdd5b
PorpanTH <porpan151@gmail.com>
2021-11-08 13:49:54 +0700

    update main

--- app.py
+++ app.py
@@ -252,5 +252,6 @@ def binary_search(arr, low, high, x):
 
 
 if __name__ == '__main__':
-
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
