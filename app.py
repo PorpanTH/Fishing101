51dedb7a559039bb577d8ad3d831a88e7ac65fcd
PorpanTH <porpan151@gmail.com>
2021-11-08 13:56:44 +0700

    host

--- app.py
+++ app.py
@@ -255,6 +255,6 @@ def binary_search(arr, low, high, x):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', port=port)
 
     # app.run()
