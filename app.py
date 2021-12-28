d2be43621b350abb5f78a55aa30d108e971804b7
PorpanTH <porpan151@gmail.com>
2021-12-28 14:44:03 +0700

    Add new page and switch to MYSQL

--- app.py
+++ app.py
@@ -8,23 +8,31 @@ import mysql.connector
 from helpers import apology, login_required
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 
-# from flask_mysqldb import MySQL
-# format the date to ddmmyyyy
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'ba74ba05397a99'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'b48cfd68'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_5e2677edc19745f'
+app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-04.cleardb.com'
+mysql.init_app(app)
+
+# app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
-# app.config['MYSQL_HOST'] = 'us-cdbr-east-04.cleardb.com'
-# app.config['MYSQL_USER'] = 'ba74ba05397a99'
-# app.config['MYSQL_PASSWORD'] = 'b48cfd68'
-# app.config['MYSQL_DB'] = 'heroku_5e2677edc19745f'
-#
-# mysql = MySQL(app)
+import functools
+def calltracker(func):
+    @functools.wraps(func)
+    def wrapper(*args):
+        wrapper.has_been_called = True
+        return func(*args)
+    wrapper.has_been_called = False
+    return wrapper
 
 @app.route("/", methods=['GET', 'POST'])
 @login_required
@@ -39,48 +47,87 @@ def index():
         NextDay_Date_Formatted = NextDay_Date.strftime('%Y-%m-%d')
 
         labels, values = graph(Current_Date_Formatted, NextDay_Date_Formatted)
+
+        # if not avg.has_been_called:
         date, fscores = avg()
 
         result = binary_search(date, 0, len(date), Current_Date_Formatted)
         fscore = fscores[result]
-        headings = ("Time", "swell height", "swell period", "wind speed", "moon phase")
-        data = getData(Current_Date_Formatted, NextDay_Date_Formatted)
+
         headings1 = ("Date", "Score")
         data1 = sugggestion()
 
-        return render_template("graph.html", labels=labels, values=values, today=Current_Date_Formatted, fscore=fscore,
-                               headings=headings, data=data, headings1=headings1, data1=data1)
+        context = {
+            'labels' : labels,
+            'values' : values,
+            'today' : Current_Date_Formatted,
+            'fscore' : fscore,
+            'headings1' : headings1,
+            'data1' : data1
+        }
+        return render_template("graph.html", **context)
     else:
         Current_Date_Formatted = datetime.datetime.today().strftime('%Y-%m-%d')  # format the date to ddmmyyyy
         NextDay_Date = datetime.datetime.today() + datetime.timedelta(days=1)
         NextDay_Date_Formatted = NextDay_Date.strftime('%Y-%m-%d')
 
         labels, values = graph(Current_Date_Formatted, NextDay_Date_Formatted)
+
+        # if not avg.has_been_called:
         date, fscores = avg()
+
         result = binary_search(date, 0, len(date), Current_Date_Formatted)
         fscore = fscores[result]
-        headings = ("Time", "swell height", "swell period", "wind speed", "moon phase")
-        data = getData(Current_Date_Formatted, NextDay_Date_Formatted)
+
         headings1 = ("Date", "Score")
         data1 = sugggestion()
 
-        return render_template("graph.html", labels=labels, values=values, today=Current_Date_Formatted, fscore=fscore,
-                               headings=headings, data=data, headings1=headings1, data1=data1)
+        context = {
+            'labels': labels,
+            'values': values,
+            'today': Current_Date_Formatted,
+            'fscore': fscore,
+            'headings1': headings1,
+            'data1': data1
+        }
+        # return render_template("graph.html", labels=labels, values=values, today=Current_Date_Formatted, fscore=fscore,
+        #                        headings=headings, data=data, headings1=headings1, data1=data1)
+        return render_template("graph.html", **context)
+        # return render_template("graph.html", labels=labels, values=values, today=Current_Date_Formatted, fscore=fscore,
+        #                        headings=headings, data=data, headings1=headings1, data1=data1)
 
+@app.route("/data", methods=['GET', 'POST'])
+def data():
+    Current_Date, i = Date.add(0)
+    Current_Date_Formatted = Current_Date.strftime('%Y-%m-%d')  # format the date to ddmmyyyy
+    NextDay_Date = Current_Date + datetime.timedelta(days=1)
+    NextDay_Date_Formatted = NextDay_Date.strftime('%Y-%m-%d')
+    headings = ("Time", "swell height", "swell period", "wind speed", "moon phase")
+    data = getData(Current_Date_Formatted, NextDay_Date_Formatted)
+
+    context = {
+        'today': Current_Date_Formatted,
+        'headings': headings,
+        'data': data
+    }
+    return render_template("weather.html", **context)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
-                                  user='ba74ba05397a99',
-                                  passwd='b48cfd68',
-                                  database='heroku_5e2677edc19745f')
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    # cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
+    #                               user='ba74ba05397a99',
+    #                               passwd='b48cfd68',
+    #                               database='heroku_5e2677edc19745f')
     if request.method == 'POST':
 
         session["email"] = request.form['email']
         username = session["email"]
         password = request.form['password']
         # cursor = mysql.connection.cursor()
-        cursor = cnx.cursor()
+
         find_user = "select * from heroku_5e2677edc19745f.user1 where username = %s AND password = %s"
         cursor.execute(find_user, (username, password))
         results = cursor.fetchall()
@@ -99,16 +146,17 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
-                                  user='ba74ba05397a99',
-                                  passwd='b48cfd68',
-                                  database='heroku_5e2677edc19745f')
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    # cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
+    #                               user='ba74ba05397a99',
+    #                               passwd='b48cfd68',
+    #                               database='heroku_5e2677edc19745f')
     if request.method == 'POST':
         found = 0
         while found == 0:
             username = request.form['username']
             find_user = "select * from heroku_5e2677edc19745f.user1 where username  ='%s'" % (username)
-            cursor = cnx.cursor()
             cursor.execute(find_user)
             records = cursor.fetchall()
             if records:
@@ -132,7 +180,7 @@ def register():
                             VALUES (%s, %s, %s, %s)"""
                 cursor.execute(insertData, (username, firstname, lastname, password))
                 session["email"] = username
-                cnx.commit()
+                conn.commit()
                 return redirect("/")
     return render_template('register.html')
 
@@ -150,12 +198,15 @@ def logout():
 
 def graph(x, y):
     print("Connecting to mysql database")
-    cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
-                                  user='ba74ba05397a99',
-                                  passwd='b48cfd68',
-                                  database='heroku_5e2677edc19745f')
-    cursor = cnx.cursor()
-    sql_select_Query = "select datetime, score from heroku_5e2677edc19745f.weather_storm where datetime BETWEEN %s AND %s"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    #
+    # cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
+    #                               user='ba74ba05397a99',
+    #                               passwd='b48cfd68',
+    #                               database='heroku_5e2677edc19745f')
+    # cursor = cnx.cursor()
+    sql_select_Query = "select datetime, score from heroku_5e2677edc19745f.weather_storm where datetime >= %s AND datetime < %s order by datetime desc"
     cursor.execute(sql_select_Query, (x, y))
     # get all records
     records = cursor.fetchall()
@@ -164,16 +215,22 @@ def graph(x, y):
     for row in records:
         labels.append(row[0])
         values.append(row[1])
+    conn.close()
+    cursor.close()
     return labels, values
 
-
+@calltracker
 def avg():
-    cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
-                                  user='ba74ba05397a99',
-                                  passwd='b48cfd68',
-                                  database='heroku_5e2677edc19745f')
-    sql_select_Query = "select * from heroku_5e2677edc19745f.average"
-    cursor = cnx.cursor()
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    # cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
+    #                               user='ba74ba05397a99',
+    #                               passwd='b48cfd68',
+    #                               database='heroku_5e2677edc19745f')
+    # cursor = cnx.cursor()
+    sql_select_Query = "select datetime, fscore from heroku_5e2677edc19745f.average"
+
     cursor.execute(sql_select_Query)
     # get all records
     records = cursor.fetchall()
@@ -182,34 +239,51 @@ def avg():
     for row in records:
         date.append(row[0].strftime('%Y-%m-%d'))
         values.append(row[1])
+    conn.close()
+    cursor.close()
     return date, values
 
 
 def getData(x, y):
-    cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
-                                  user='ba74ba05397a99',
-                                  passwd='b48cfd68',
-                                  database='heroku_5e2677edc19745f')
-    sql_select_Query = "select * from heroku_5e2677edc19745f.weather_storm where datetime BETWEEN %s AND %s"
-    cursor = cnx.cursor()
-    cursor.execute(sql_select_Query, (x, y))
+    # cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
+    #                               user='ba74ba05397a99',
+    #                               passwd='b48cfd68',
+    #                               database='heroku_5e2677edc19745f')
+    # cursor = cnx.cursor()
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    sql_select_Query = "select * from heroku_5e2677edc19745f.weather_storm where datetime BETWEEN %(date1)s AND %(date2)s"
+    data = {
+        'date1' : x,
+        'date2' : y
+    }
+    cursor.execute(sql_select_Query, data)
     # get all records
 
     records = cursor.fetchall()
+    conn.close()
+    cursor.close()
     return records
 
 
 def sugggestion():
-    cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
-                                  user='ba74ba05397a99',
-                                  passwd='b48cfd68',
-                                  database='heroku_5e2677edc19745f')
+    # cnx = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
+    #                               user='ba74ba05397a99',
+    #                               passwd='b48cfd68',
+    #                               database='heroku_5e2677edc19745f')
+    # cursor = cnx.cursor()
     # sql_select_Query = "select * from heroku_5e2677edc19745f.average where MONTH(datetime) = MONTH(CURDATE()), order by fscore desc limit 3 "
     # sql_select_Query = "select * from heroku_5e2677edc19745f.average order by fscore desc limit 3 "
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
     sql_select_Query = "select * from heroku_5e2677edc19745f.average where MONTH(datetime) = MONTH(CURDATE()) order by fscore desc limit 3 "
-    cursor = cnx.cursor()
+
     cursor.execute(sql_select_Query)
     records = cursor.fetchall()
+    conn.close()
+    cursor.close()
     return records
 
 
