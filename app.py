95b2ff6bc1b345db1cf076f7641ff0e71d802839
PorpanTH <porpan151@gmail.com>
2022-02-14 00:15:52 +0700

    session permanent

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-04.cleardb.com'
 mysql.init_app(app)
 # app.config['SECRET_KEY'] = os.urandom(24)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 app.config['SESSION_COOKIE_NAME'] = "my_session"
 Session(app)
