dc31a713e04aca1538ebdcb66daab05fd678e426
PorpanTH <porpan151@gmail.com>
2022-02-13 23:31:43 +0700

    expire

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ Session(app)
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "public, no-store,max-age=604800, must-revalidate"
-    response.headers["Expires"] = 6000
+    response.headers["Expires"] = 0
     # response.headers["Pragma"] = "no-cache"
     return response
 
