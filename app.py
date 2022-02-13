98925f6ec1102d099a259b2457da426d531394a6
PorpanTH <porpan151@gmail.com>
2022-02-13 23:35:53 +0700

    Cache header

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ Session(app)
 
 @app.after_request
 def after_request(response):
-    response.headers["Cache-Control"] = "public, no-store,max-age=604800, must-revalidate"
-    response.headers["Expires"] = 0
+    response.headers["Cache-Control"] = "public, no-store,max-age=6000"
+    # response.headers["Expires"] = 0
     # response.headers["Pragma"] = "no-cache"
     return response
 
